b9cd4840829677bccb9c8b96cc8c6dbecb4470a9
Raul Santos <raulsantos.comic@gmail.com>
2020-01-19 14:55:58 -0300

    Icon Content

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import styled from 'styled-components';
 import BkgIlust from './BkgIlust.svg';
 import TrazPraMimBtn from './TrazPraMimBtn.svg';
+import Icon1 from './Icon1.svg';
+import Icon2  from './Icon2.svg';
+import Icon3 from './Icon3.svg';
 
 const StyledLogo = styled.div`
   padding: 35px 0px 0px 140px;
@@ -32,7 +35,7 @@ const MenuItem = styled.div`
 `
 
 const Backgroundimg = styled.div`
-  padding: 20px 0px 0px 0px;
+  padding: 10px 0px 0px 0px;
 `
 
 const TrazPraMimBox = styled.div`
@@ -56,6 +59,32 @@ const Text = styled.div`
   font-weight: regular;
   opacity: 0.8;
   padding: 0px 0px 10px 8px;
+`
+
+const SecondBkg = styled.div`
+  background-color: #FFF5CC;
+  width: 100vw;
+  height: 100vh;
+`
+
+const ContentIcons = styled.div`
+  display: flex; 
+  flex-direction: row;
+  justify-content: center;
+  padding: 90px 0px 0px 0px;
+`
+
+const ColumnCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding:  0px 150px 0px 150px;
+ `
+
+const TextCont = styled.div`
+  font-size: 30px;
+  font-weight: regular;
+  text-align: center;
+  padding: 50px 0px 0px 0px;
 `
 
 export const Landing = () => {
@@ -88,7 +117,36 @@ export const Landing = () => {
           </Text>
           <img src={TrazPraMimBtn} alt="" />
         </TrazPraMimBox>
-      </BkgGradient>  
+      </BkgGradient> 
+      <SecondBkg>
+        <ContentIcons>
+          <ColumnCont> 
+            <img src={Icon1} alt=""></img> 
+            <TextCont>
+              Lorem ipsum<br/>
+              dolor sit amet,<br/>
+              consectetur 
+            </TextCont>
+          </ColumnCont>
+          <ColumnCont> 
+            <img src={Icon2} alt=""></img> 
+            <TextCont>
+              Lorem ipsum<br/>
+              dolor sit amet,<br/>
+              consectetur 
+            </TextCont>
+          </ColumnCont>
+          <ColumnCont> 
+            <img src={Icon3} alt=""></img> 
+            <TextCont>
+              Lorem ipsum<br/>
+              dolor sit amet,<br/>
+              consectetur 
+            </TextCont>
+          </ColumnCont>
+        
+        </ContentIcons>
+        </SecondBkg> 
     </Fragment> 
             
   );
